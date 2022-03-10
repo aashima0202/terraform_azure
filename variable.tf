69e20f6e3e39ae3ec9f710c8cd58b411c7d8235c
aashima0202 <99241751+aashima0202@users.noreply.github.com>
2022-03-10 19:14:45 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -2,12 +2,7 @@ variable "resource_group_location" {
   default = "eastus"
   description   = "Location of the resource group."
 }
-variable "rg_dev" {
-  default       = "rg_aashima_dev"
-}
-variable "rg_qa" {
-  default       = "rg_aashima_qa"
-}
+
 variable "rg_preprod" {
   default       = "preprod_var"
 }
